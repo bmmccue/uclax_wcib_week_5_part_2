94bfe2d692a993817f05a645ca5790b87240c812
Apple <apple@apples-air.home>
2020-11-03 18:44:06 -0800

    javascript changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -7,35 +10,29 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+// 2. RAW TEXT STRINGS
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-the soup kitchen
-Disneyland
-the White House
-
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
-
 function result() {
-
+  
   if(customName.value !== '') {
-    let name = customName.value;
+      let name = customName.value;
 
+    }
+
+    if(document.getElementById("uk").checked) {
+      let weight = Math.round(300);
+      let temperature =  Math.round(94);
+
+    }
+
+    story.textContent = ;
+    story.style.visibility = 'visible';
   }
-
-  if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
-}
