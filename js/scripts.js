6accc9f1aaeb2c72445bb517cfac6d085976e64b
Apple <apple@apples-air.home>
2020-11-04 10:17:04 -0800

    more js changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 // 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
@@ -12,7 +11,7 @@ function randomValueFromArray(array){
 
 // 2. RAW TEXT STRINGS
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:,they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
@@ -20,8 +19,14 @@ let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk'
 // 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
+
 function result() {
-  
+
+  var newStory = "storyText";
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
   if(customName.value !== '') {
       let name = customName.value;
 
