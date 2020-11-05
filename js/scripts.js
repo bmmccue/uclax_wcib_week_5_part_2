89c893328fa7d54efeac0397a36a7c8098c34816
Apple <apple@apples-air.home>
2020-11-05 12:45:50 -0800

    more js changes

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function randomValueFromArray(array){
 
 // 2. RAW TEXT STRINGS
 
-var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:,they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:,they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
@@ -23,23 +23,23 @@ randomize.addEventListener('click', result);
 function result() {
 
   let newStory = "storyText";
-  var xItem = randomValueFromArray(insertX);
-  var yItem = randomValueFromArray(insertY);
-  var zItem = randomValueFromArray(insertZ);
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
 
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',yItem);
   newStory = newStory.replace(':insertx:',zItem);
 
   if(customName.value !== '') {
-      let name = customName.value;
-      newStory = newStory.replace('Bob','name');
+      const name = customName.value;
+      newStory = newStory.replace('Bob',name);
 
     }
 
     if(document.getElementById("uk").checked) {
-      let weight = Math.round(300*0.0714286) + 'stone';
-      let temperature =  Math.round((94-32)*5/9) + 'centigrade';
+      const weight = Math.round(300*0.0714286) + 'stone';
+      const temperature =  Math.round((94-32)*5/9) + 'centigrade';
       newStory = newStory.replace ('94 fahrenheit', 'temperature');
       newStory = newStory.replace ('300 pounds', 'weight');
 
